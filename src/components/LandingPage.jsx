02433e7c2a94d8f41789c808be2b227adfe88724
Esi Yankey <graceyan244@gmail.com>
2023-08-01 05:29:15 +0000

    chnge

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -4,6 +4,8 @@ import { FaFacebookF, FaInstagram } from "react-icons/fa";
 import { GoTriangleDown } from "react-icons/go";
 import { RiTwitterFill } from "react-icons/ri";
 import { HiOutlineMenuAlt2 } from "react-icons/hi";
+import {IoUnlinkSharp} from "react-icons/io"
+import {AiOutlineHeart} from "react-icons/ai"
 import "../styles/landingPage.css";
 
 export const LandingPage = () => {
@@ -85,6 +87,25 @@ export const LandingPage = () => {
             <h2>e2woo & aiwibi Collections</h2>
             <p>Here you will find baby and children's clothes, shoes ,toys,strollers,car seats,maternity clothes,acessories and more.</p>
             <button className="btn">Shop Now</button>
+        </div>
+      </div>
+      <div className="newest-products">
+        <h4>Newest Products</h4>
+      </div>
+      <div className="grid-images">
+        <div className="grid-container">
+          <div className="card-images">
+            <img src="https://shopkiddieboo.com/wp-content/uploads/2021/09/1-1821-300x300.jpg" alt="" />
+            <div className="text">
+              <h5>this is one image</h5>
+            </div>
+            <div className="hover">
+              <BiCart/>
+              {/* <HiOutlineMagnifyingGlassPlus/> */}
+              <AiOutlineHeart/>
+              <IoUnlinkSharp/>
+            </div>
+          </div>
         </div>
       </div>
     </>
